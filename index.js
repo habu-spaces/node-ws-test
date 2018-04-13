86ebcd66a0d70122f209a5a8f27b36164e44318b
David B <dromar56@gmail.com>
2018-04-13 16:55:27 +0100

    test

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
 
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
 server.listen(port)
